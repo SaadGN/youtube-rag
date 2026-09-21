e530ca321680e6c1ed6e0657d41ec7017f5c328f
SaadGN <saadgn89@gmail.com>
2026-09-21 13:22:03 +0500

    feat/complete chain created

--- src/rag/chain.py
+++ src/rag/chain.py
@@ -1,4 +1,8 @@
-
+from langchain_core.runnables import (
+    RunnableParallel,
+    RunnablePassthrough,
+    RunnableLambda
+)
 from langchain_core.output_parsers import StrOutputParser
 from .prompt import RAG_PROMPT
 from src.llm.model import get_llm
@@ -12,3 +16,29 @@ def format_docs(documents):
         for document in documents
     )
 
+def create_rag_chain(retriever):
+    """
+    Create the complete LCEL RAG chain
+    """
+
+    llm = get_llm()
+
+    parallel_chain = RunnableParallel(
+        {
+            "context": (
+                retriever
+                | RunnableLambda(format_docs)
+            ),
+            "question": RunnablePassthrough()
+        }
+    )
+
+    rag_chain = (
+        parallel_chain
+        | RAG_PROMPT
+        | llm
+        | StrOutputParser()
+
+    )
+
+    return rag_chain
